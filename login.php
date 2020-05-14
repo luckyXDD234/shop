0f2210954b76d73142879305f1e35481502830d6
luckyXDD234 <60270393+luckyXDD234@users.noreply.github.com>
2020-05-15 03:10:36 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if(!empty($name) || !empty($password)){
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "tom's shop";
+
+// create connection keyword mysqli
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+if (mysqli_connect_error()){
+    die('connection error! Please try again later('. mysqli_connect_errno().'))'. mysqli_connect_error());
+} else {
+    
+    $result = mysqli_query("select * from customer where email = '$email' and password = '$password'");
+    $row = mysql_fetch_array($result);
+    echo "row";
+    if (($row['email'] == $email) && ($row['password'] == $password)){
+        echo "Login in success!"; 
+    } else {
+        echo "Login in failed";
+    }
+
+
+    
+}
+}
+?>
+
+
--- register.php
+++ register.php
@@ -1,28 +1,48 @@
 <?php
-$name = $_POST['name']    
-$phone = $_POST['phone']
-$gender = $_POST['gender']
-$age = $_POST['age']
-$email = $_POST['email']
-$password = $_POST['password']
+$name = $_POST['name'];    
+$phone = $_POST['phone'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 if(!empty($name) || !empty($phone) || !empty($gender) || !empty($age) ||
  !empty($email) || !empty($password)){
     $host = "localhost";
     $dbUsername = "root";
     $dbPassword = "";
-    $dbname = "Tom's shop";
+    $dbname = "tom's shop";
 
 // create connection keyword mysqli
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
+    if (mysqli_connect_error()){
+        die('connection error! Please try again later('. mysqli_connect_errno().'))'. mysqli_connect_error());
+    } else {
+        $SELECT = "Select email from customer where email = ? Limit 1";
+        $INSERT = "Insert into customer(name, phone, gender, age, email, password)
+        value(?, ?, ?, ?, ?, ?)";
+    
+        //prepare statement
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
 
-
+            if ($rnum==0){
+                $stmt->close();
+                $stmt = $conn->prepare($INSERT);
+                $stmt->bind_param("sisiss", $name, $phone, $gender, $age, $email, $password);
+                $stmt->execute();
+                echo "Your have registered successfully.";
+            }else{
+                echo "This email has already been registered.";
+            }
+    }
  }  else{
-     echo "Please enter all the information!"
+     echo "Please enter all the information!";
      die();
  }
-
-
-
-
 ?>
